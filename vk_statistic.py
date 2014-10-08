ec52d3e0c3f2ed02dff009c82812a8558a961952
Anna Svalova <a.svalova@ideco.ru>
2014-10-08 15:59:01 +0600

    Написан vk_statistic. Надо доделать

--- vk_statistic.py
+++ vk_statistic.py
@@ -4,12 +4,12 @@ from urllib.request import urlopen
 import random
 
 #Количество пользователей в выборке, по которой бдет оцениваться генеральная совокупность
-num_in_sample = 5000
+num_in_sample = 1
 subscript = {}
 
 #Основной цикл, с помощью которого собирается статистика
 for i in range(num_in_sample):
-    uid = str(int(random.random() * 2147483646) + 1)
+    uid = str(int(random.random() * 21000000) + 1)
     url = 'http://api.vk.com/method/users.getSubscriptions.json?uid=' + uid
     try:
         data = urlopen(url).read().decode('utf8')
@@ -17,8 +17,9 @@ for i in range(num_in_sample):
     except Exception:
         i -= 1
         continue
-
+    print(uid)
     users = data['response']['users']['items']
+    print(users)
     for user in users:
         if user in subscript.keys():
             print('OK')
