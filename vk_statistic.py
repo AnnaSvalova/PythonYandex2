d12d82f082337d331f659c7fa6823e03dabda0b7
Anna Svalova <a.svalova@ideco.ru>
2014-10-08 22:34:27 +0600

    Написан vk_statistic. Надо доделать

--- vk_statistic.py
+++ vk_statistic.py
@@ -4,12 +4,14 @@ from urllib.request import urlopen
 import random
 
 #Количество пользователей в выборке, по которой бдет оцениваться генеральная совокупность
-num_in_sample = 1
+num_in_sample = 500
 subscript = {}
+max = 0
 
 #Основной цикл, с помощью которого собирается статистика
-for i in range(num_in_sample):
-    uid = str(int(random.random() * 21000000) + 1)
+i = 0
+while i < num_in_sample:
+    uid = str(int(random.random() * 270000000) + 1)
     url = 'http://api.vk.com/method/users.getSubscriptions.json?uid=' + uid
     try:
         data = urlopen(url).read().decode('utf8')
@@ -17,23 +19,28 @@ for i in range(num_in_sample):
     except Exception:
         i -= 1
         continue
-    print(uid)
     users = data['response']['users']['items']
-    print(users)
+    if users.__len__() == 0:
+        i -= 1
+        continue
     for user in users:
         if user in subscript.keys():
-            print('OK')
             subscript[user] += 1
         else:
             subscript[user] = 1
+        if subscript[user] > max:
+            max = subscript[user]
+    i += 1
 
+print(max)
 output = []
 for id in subscript.keys():
     output.append([subscript[id], id])
+print(output)
 
 #Вывод самых популярных пользователей среди просмотренных
 output = sorted(output)
 for i in range(30):
     print('id' + str(output[i][1]) + ' ' + str(output[i][0]))
 
-print(subscript)
+#print(subscript)
