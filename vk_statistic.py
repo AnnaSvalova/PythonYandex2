d81c2237d1990c374635e6d7260d872bfc7b9d68
Anna Svalova <a.svalova@ideco.ru>
2014-10-09 14:46:09 +0600

    Исправлен vk_statistic.py. Теперь правильно собирается статистика и делается нормальный вывод

--- vk_statistic.py
+++ vk_statistic.py
@@ -2,11 +2,26 @@
 import json
 from urllib.request import urlopen
 import random
+import os
 
-#Количество пользователей в выборке, по которой бдет оцениваться генеральная совокупность
-num_in_sample = 500
+#Для того, чтобы придать программе более эстетичный вид, почистим консоль и уберем курсор.
+try:
+    os.system('clear')
+except Exception:
+    os.system('cls')
+try:
+    os.system('setterm -cursor off')
+except Exception:
+    pass
+
+#Количество пользователей в выборке, по которой бдет оцениваться генеральная совокупность. Все uid генерируются
+#псевдослучайным образом.
+num_in_sample = 5000
+one_percent = num_in_sample // 100
+percent = 1
 subscript = {}
-max = 0
+
+print('  0% done', end='')
 
 #Основной цикл, с помощью которого собирается статистика
 i = 0
@@ -17,30 +32,43 @@ while i < num_in_sample:
         data = urlopen(url).read().decode('utf8')
         data = json.loads(data)
     except Exception:
-        i -= 1
         continue
     users = data['response']['users']['items']
     if users.__len__() == 0:
-        i -= 1
         continue
     for user in users:
         if user in subscript.keys():
             subscript[user] += 1
         else:
             subscript[user] = 1
-        if subscript[user] > max:
-            max = subscript[user]
     i += 1
+    if i % one_percent == 0:
+        print('\r%3d' % percent, end='')
+        percent += 1
 
-print(max)
+print()
+
 output = []
 for id in subscript.keys():
     output.append([subscript[id], id])
-print(output)
 
 #Вывод самых популярных пользователей среди просмотренных
-output = sorted(output)
-for i in range(30):
-    print('id' + str(output[i][1]) + ' ' + str(output[i][0]))
+output = sorted(output)[-30:]
+for user in output:
+    try:
+        uid = user[1]
+        url = 'http://api.vk.com/method/users.get.json?uid=' + str(uid)
+        data = urlopen(url).read().decode('utf8')
+        data = json.loads(data)
+    except Exception:
+        continue
+    print('id%-9d    %s %s' % (uid, data['response'][0]['first_name'], data['response'][0]['last_name']))
 
-#print(subscript)
+#Возвращаем курсор
+try:
+    os.system('setterm -cursor on')
+except Exception:
+    pass
+
+#TODO сделать скрытие курсора в Windows
+#TODO посчитать нормальное количество элементов в выборке для нормальной оценки генеральной совокупности
